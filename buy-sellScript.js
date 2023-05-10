5745938d253c55493a3ab84fed0b97f3fe140a55
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-11 00:30:39 +0700

    first commit

---
+++ buy-sellScript.js
@@ -0,0 +1,8 @@
+function BuyPage() {
+	SwitchBuy.style.border = '0.2vw solid aquamarine'
+    SwitchSell.style.border = '0'
+}
+function SellPage() {
+	SwitchBuy.style.border = '0'
+    SwitchSell.style.border = '0.2vw solid aquamarine'
+}
