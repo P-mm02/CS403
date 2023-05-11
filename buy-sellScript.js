e1a3ba7cb87f81e1ae7a2cceb60a798cf7329556
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-11 07:01:28 +0700

    buy

--- buy-sellScript.js
+++ buy-sellScript.js
@@ -1,8 +1,12 @@
 function BuyPage() {
 	SwitchBuy.style.border = '0.2vw solid aquamarine'
     SwitchSell.style.border = '0'
+    SwitchBuyPage.style.visibility = 'visible'
+    SwitchSellPage.style.visibility = 'hidden'
 }
 function SellPage() {
 	SwitchBuy.style.border = '0'
     SwitchSell.style.border = '0.2vw solid aquamarine'
+    SwitchBuyPage.style.visibility = 'hidden'
+    SwitchSellPage.style.visibility = 'visible'
 }
