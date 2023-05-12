dcb83012208108852b4c00b21fad1fc400c2643a
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-12 09:39:02 +0700

    add

---
+++ buyScript.js
@@ -0,0 +1,13 @@
+function BuyWindow(){
+    OpenBuyWindow.style.visibility = 'visible'
+}
+function CloseBuyWindow(){
+    OpenBuyWindow.style.visibility = 'hidden'
+}
+function BuyWindow2(){
+    OpenBuyWindow2.style.left = '30vw'
+    OpenBuyWindow2.style.visibility = 'visible'
+}
+function CloseBuyWindow2(){
+    OpenBuyWindow2.style.visibility = 'hidden'
+}
