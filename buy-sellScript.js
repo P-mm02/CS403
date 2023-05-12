81fedaaa4325916fc38d64cd76f6f467a316d804
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-13 06:53:10 +0700

    add

--- buy-sellScript.js
+++ buy-sellScript.js
@@ -10,30 +10,14 @@ function SellPage() {
     SwitchBuyPage.style.visibility = 'hidden'
     SwitchSellPage.style.visibility = 'visible'
 }
-const selectImage = document.querySelector('.select-image');
-const inputFile = document.querySelector('#file');
-const imgArea = document.querySelector('.img-area');
 
-selectImage.addEventListener('click', function () {
-	inputFile.click();
-})
+let imgInput = document.querySelector("#selectImg1")
+let previewImg =document.querySelector("#previewImg1")
 
-inputFile.addEventListener('change', function () {
-	const image = this.files[0]
-	if(image.size < 2000000) {
-		const reader = new FileReader();
-		reader.onload = ()=> {
-			const allImg = imgArea.querySelectorAll('img');
-			allImg.forEach(item=> item.remove());
-			const imgUrl = reader.result;
-			const img = document.createElement('img');
-			img.src = imgUrl;
-			imgArea.appendChild(img);
-			imgArea.classList.add('active');
-			imgArea.dataset.img = image.name;
+imgInput.onchange = evt => {
+	const[file] = imgInput.files
+		if (file) {
+			previewImg.src = URL.createObjectURL(file)
 		}
-		reader.readAsDataURL(image);
-	} else {
-		alert("Image size more than 2MB");
-	}
-})
+	
+}
