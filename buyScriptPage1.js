2dc560e8b2290830b2f1ce3493f5e3b3c534da2d
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-14 08:44:58 +0700

    fix

--- buyScriptPage1.js
+++ buyScriptPage1.js
@@ -1,10 +1,46 @@
 function BuyWindow11(){
     BuyWindowPage1.style.left = '30vw'
     BuyWindowPage1.style.visibility = 'visible'
+
+    fetch('buyData.json')
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        appenData(data)
+    })
+    .catch(function(err){
+        console.log('error: ' + err)
+    })
+    
+    function appenData(data){
+        var mainContainer = document.getElementById("BuyWindowPage1")  
+        var spanTime = document.createElement("span")
+        spanTime.innerHTML = "Registeration Time: <br>" + data[0].regisTime
+        mainContainer.appendChild(spanTime)
+    
+        var spanName = document.createElement("span")
+        spanName.innerHTML = "Name: <br>" + data[0].name    
+        mainContainer.appendChild(spanName)
+    
+        var spanGameName = document.createElement("span")
+        spanGameName.innerHTML = "Game: <br>" + data[0].gameName    
+        mainContainer.appendChild(spanGameName)
+        
+        var spanPrice = document.createElement("span")
+        spanPrice.innerHTML = "Price: <br>" + data[0].price    
+        mainContainer.appendChild(spanPrice)
+    
+        var spanAddress = document.createElement("span")
+        spanAddress.innerHTML = "Address: <br>" + data[0].address    
+        mainContainer.appendChild(spanAddress)   
+          
+    }
 }
 function CloseBuyWindow11(){
     BuyWindowPage1.style.visibility = 'hidden'
 }
+
 fetch('buyData.json')
     .then(function(response){
         return response.json()
@@ -16,28 +52,8 @@ fetch('buyData.json')
         console.log('error: ' + err)
     })
 
-function appenData(data){
-    var mainContainer = document.getElementById("BuyWindowPage1")  
-    var spanTime = document.createElement("span")
-    spanTime.innerHTML = "Registeration Time: <br>" + data[0].regisTime
-    mainContainer.appendChild(spanTime)
-
-    var spanName = document.createElement("span")
-    spanName.innerHTML = "Name: <br>" + data[0].name    
-    mainContainer.appendChild(spanName)
-
-    var spanGameName = document.createElement("span")
-    spanGameName.innerHTML = "Game: <br>" + data[0].gameName    
-    mainContainer.appendChild(spanGameName)
-    
-    var spanPrice = document.createElement("span")
-    spanPrice.innerHTML = "Price: <br>" + data[0].price    
-    mainContainer.appendChild(spanPrice)
-
-    var spanAddress = document.createElement("span")
-    spanAddress.innerHTML = "Address: <br>" + data[0].address    
-    mainContainer.appendChild(spanAddress)     
-
+function appenData(data){ 
+    var mainContainer = document.getElementById("BuyWindowPage1")   
     var mainContainer1 = document.getElementById("datePage11")
     const datePage11 = document.getElementById("datePage11")
     datePage11.innerHTML = "Registeration Time: <br>" + data[0].regisTime
@@ -56,4 +72,5 @@ function appenData(data){
     mainContainer2.appendChild(datePage12)
     
 }
+
 
--- buyScriptPage2.js
+++ buyScriptPage2.js
@@ -1,11 +1,8 @@
 function BuyWindow2(){
     BuyWindowPage2.style.left = '30vw'
     BuyWindowPage2.style.visibility = 'visible'
-}
-function CloseBuyWindow2(){
-    BuyWindowPage2.style.visibility = 'hidden'
-}
-fetch('buyData.json')
+
+    fetch('buyData.json')
     .then(function(response){
         return response.json()
     })
@@ -16,25 +13,32 @@ fetch('buyData.json')
         console.log('error: ' + err)
     })
 
-function appenData(data){
-    var mainContainer2 = document.getElementById("BuyWindowPage2")  
-    var spanTime2 = document.createElement("span")
-    spanTime2.innerHTML = "Registeration Time: <br>" + data[0].regisTime
-    mainContainer2.appendChild(spanTime2)
-
-    var spanName2 = document.createElement("span")
-    spanName2.innerHTML = "Name: <br>" + data[0].name    
-    mainContainer2.appendChild(spanName2)
-
-    var spanGameName2 = document.createElement("span")
-    spanGameName2.innerHTML = "Game: <br>" + data[0].gameName    
-    mainContainer2.appendChild(spanGameName2)
+    function appenData(data){
+        var mainContainer2 = document.getElementById("BuyWindowPage2")  
+        var spanTime2 = document.createElement("span")
+        spanTime2.innerHTML = "Registeration Time: <br>" + data[0].regisTime
+        mainContainer2.appendChild(spanTime2)
     
-    var spanPrice2 = document.createElement("span")
-    spanPrice2.innerHTML = "Price: <br>" + data[0].price    
-    mainContainer2.appendChild(spanPrice2)
-
-    var spanAddress2 = document.createElement("span")
-    spanAddress2.innerHTML = "Address: <br>" + data[0].address    
-    mainContainer2.appendChild(spanAddress2)
+        var spanName2 = document.createElement("span")
+        spanName2.innerHTML = "Name: <br>" + data[0].name    
+        mainContainer2.appendChild(spanName2)
+    
+        var spanGameName2 = document.createElement("span")
+        spanGameName2.innerHTML = "Game: <br>" + data[0].gameName    
+        mainContainer2.appendChild(spanGameName2)
+        
+        var spanPrice2 = document.createElement("span")
+        spanPrice2.innerHTML = "Price: <br>" + data[0].price    
+        mainContainer2.appendChild(spanPrice2)
+    
+        var spanAddress2 = document.createElement("span")
+        spanAddress2.innerHTML = "Address: <br>" + data[0].address    
+        mainContainer2.appendChild(spanAddress2)
+    }
 }
+function CloseBuyWindow2(){
+    BuyWindowPage2.style.visibility = 'hidden'
+}
+
+
+
